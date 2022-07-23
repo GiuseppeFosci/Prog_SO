7320f086c8152d92662aa406cefc638189c43c3d
aludra <bocancea.1764205@studenti.uniroma1.it>
2022-07-23 13:51:43 +0200

    versione funzionante per misurare un segnale analogico

--- oscilloscope_avr.c
+++ oscilloscope_avr.c
@@ -9,11 +9,12 @@
 int main(void){
 
  // Inizializzazione printf/uart
-  printf_init(); 
+  printf_init();
+  
  //Configuro i vari PIN come input, sono i bit 0:7 della porta F
-
   const uint8_t mask=0xFF; 
   DDRF  &= ~mask;
+  
   //Imposto valore di default dei PIN basso (0V)
   PORTF &= ~mask;
 
@@ -21,47 +22,30 @@ int main(void){
    Lasciando però inalterato il valore degli altri bit di PRR0*/
    const uint8_t mask_PRADC =0x01; 
   PRR0 &= ~mask_PRADC;
-  printf("PRADC Value :  %x\n", (int) PRADC);
 
-  //ADC enabled settando ADC Enable bit (ADEN) ad 1, in ADCSRA. ADEN è il bit 7 di ADCSRA
-  const uint8_t mask_ADEN = 0x80;
-  ADCSRA |= mask_ADEN; 
+  
   /*Scelta canale analogico - MUX 4 in ADMUX e MUX5 in ADCRSB - 00000000 per ADC0*/
   ADCSRB = 0x00;
   ADMUX = 0x00;
-   if(ADCSRB==0x00 && ADMUX==0x00){
-      printf("Conversione sul canale scelto ADC0\n");
-    }
-  //Abilitazione ADC Interrupt Enable bit (ADIE) in ADCSRA
-  const uint8_t mask_ADIE = 0x08;
-  ADCSRA |= mask_ADIE;
-  //Inoltre bisogna attivare Global Interrupt Enable Bit - (BIT 7-I) in SREG - AVR Status Register
+  //Giuseppe
+  
+  //Michele
+  //imposto ADCSRA, ovvero abilito l'ADC ponendo ADEN = 1, pongo il flag ADIF=1 per dire che non sta convertendo attualmente, 
+  //configuro il clock dell'ADC con cui viene campionato in base alla frequenza dell'arduinomega2650
+  ADCSRA = 0x97;
 
-  const uint8_t mask_Gint = 0x80;
-  SREG |= mask_Gint;
-  //impostazione voltaggio riferimento REFS1:0 in ADMUX.
-  ADMUX |= 0xc0;
-
+  //imposto il voltaggio di riferimento di ADMUX = AVCC e modalità right aligned.
+  ADMUX = 0x40;
+  //Michele
 
   while(1){
-    //Voglio leggere gli 8 bit in input
-    const uint8_t input_bit = PINF;
-    printf("PINF :  %x\n", (int) input_bit);
-    /*La conversione inizia se setto ADC Auto Trigger Enable bit. ADATE in ADATE in ADCSRA.
-    ADATE è il BIT 5 di ADCSRA*/
-    const uint8_t mask_ADATE = 0x20;
-    ADCSRA |= mask_ADATE;
-    //IMPOSTO ADLAR - bit 5 in ADMUX per risultato con left adjustment
-    ADMUX |=0x20;
-    //Lettura risultato su ADC
-    if(ADIF==1){ //Se ADIF è 1, la conversione è terminata
-
-    printf("Conversion Value :  %lf\n", (double) ADCH);
-
-    }
-
-    _delay_ms(1000); // from delay.h, wait 1 sec
-
-
+	//Gabriele e Michele
+    const uint8_t mask_ADSC = 0x40;
+    //Faccio iniziare la conversione ponendo ADSC = 1
+    ADCSRA |= mask_ADSC;
+    while(ADIF==0){ /*Se ADIF = 1, la conversione è terminata, perciò se ADIF = 0 aspetto finché non finisce la conversione */ }
+    printf("Valore di conversione (1024=5V, 0=0V) :  %d\n", (int) ADC);
+    _delay_ms(1000);
+    //Gabriele e Michele
   }
 }
