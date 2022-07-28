b9715c5a273c2aa4b9a5b4ed522c7fe3411c73fc
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2022-07-28 22:22:27 +0200

    Gestione inizio misurazione e fine misurazione tramite USART interrupt (senza busy waiting nell'invio dei dati dal PC)

--- oscilloscope_avr.c
+++ oscilloscope_avr.c
@@ -1,59 +1,92 @@
 #include <util/delay.h>
 #include <stdio.h>
-#include "avr_common/uart.h" // Inizializza seriale per supportare printf
+#include "avr_common/uart.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdint.h> //Mi permette di definire interi a 8, 16 o 32 bit
-//Michele
-//Ogni volta che c'è la fine di una conversione viene avviato un interrupt e salvata la misura in una variabile
-volatile uint16_t misura;
-ISR(ADC_vect){
-	misura=ADC;
+#include <stdint.h>
+#include <string.h>
+#define BAUD 9600
+#define MYUBRR (F_CPU/16/BAUD-1)
+#define MAX_BUF 256
+
+char str_to_get[MAX_BUF];
+volatile uint8_t interrupt_flag = 0;
+volatile uint8_t measurement;
+
+void UART_init(void){
+	UBRR0H = (uint8_t)(MYUBRR>>8);
+	UBRR0L = (uint8_t)MYUBRR;
+	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0);
 }
-//Michele
 
-//Giuseppe
-int main(void){
- // Inizializzazione printf/uart
-  printf_init();
-  //disabilito tutti gli interrupt
-  cli();
- //Configuro i vari PIN come input, sono i bit 0:7 della porta F
-  const uint8_t mask=0xFF; 
-  DDRF  &= ~mask;
-  
-  //Imposto valore di default dei PIN basso (0V)
-  PORTF &= ~mask;
+void UART_putChar(uint8_t c){
+  while ( !(UCSR0A & (1<<UDRE0)) );
+  UDR0 = c;
+}
 
-  /*Disabilito Power Reduction ADC Bit  (PRADC) nel registro PRR0 (Power REduction Register)
-   Lasciando però inalterato il valore degli altri bit di PRR0*/
-   const uint8_t mask_PRADC =0x01; 
-  PRR0 &= ~mask_PRADC;
-
-  
-  /*Scelta canale analogico - MUX 4 in ADMUX e MUX5 in ADCRSB - 00000000 per ADC0*/
-  ADCSRB = 0x00;
-  ADMUX = 0x00;
-  //Giuseppe
-  
-  //Michele
-  //imposto ADCSRA, ovvero abilito l'ADC ponendo ADEN = 1, pongo il flag ADIF=1 per dire che la conversione non è in corso, 
-  //configuro il clock dell'ADC con cui viene campionato in base alla frequenza dell'arduinomega2650
-  //abilitiamo anche l'interrupt ponendo ADIE=1
-  ADCSRA = 0x9F;
-
-  //imposto il voltaggio di riferimento di ADMUX = AVCC e modalità right aligned.
-  ADMUX = 0x40;
-  //Michele
-  //riabilito gli interrupt
-  sei();
-  while(1){
-	//Giuseppe e Michele
-    const uint8_t mask_ADSC = 0x40;
-    //Faccio iniziare la conversione ponendo ADSC = 1
-    ADCSRA |= mask_ADSC;
-    printf("Valore di conversione (1024=5V, 0=0V) :  %d\n", (int) misura);
-    _delay_ms(100);
-    //Giuseppe e Michele
+void UART_putString(uint8_t* buf){
+  while(*buf){
+    UART_putChar(*buf);
+    ++buf;
   }
 }
+//Michele
+ISR(USART0_RX_vect){
+	uint8_t c=UDR0;
+	if(c == 'y'){	//y per far partire l'ADC
+			ADCSRA = 0xBF;
+			ADCSRA |= 0x40;
+	}
+	if(c == 'n'){	//n per far fermare l'ADC
+			ADCSRA = 0x00;
+			ADCSRA = 0xBF;
+	}
+}
+//Michele
+
+//Gabriele e Michele
+ISR(ADC_vect){
+	measurement=(uint8_t)ADC;
+	interrupt_flag=1;
+}
+//Gabriele e Michele
+
+int main(void){
+	//Gabriele e Michele
+	cli();
+	UART_init();
+	ADCSRB = 0x00;
+	ADMUX = 0x40;
+	sei();
+	while(1){
+		if(interrupt_flag){
+		sprintf(str_to_get, "%d\n",(int)measurement);
+		UART_putString((uint8_t*)str_to_get);
+		interrupt_flag=0;
+		}
+	}
+	//Gabriele e Michele
+}
+/* If you want to put measurements into a file
+ * write this on the terminal.
+ * 
+ * 1.Put the process of writing on file in background
+ * cat /dev/ttyXXXX > text.txt &     
+ * 
+ * 
+ * 2.Start writing on the file
+ * echo "y">/dev/ttyXXXX
+ * 
+ * 
+ * 3.Stop writing on the file 
+ * echo "n">/dev/ttyXXXX
+ * 
+ * 
+ * 4.To see what are the measurements on the terminal
+ * cat /dev/ttyXXXX (only after 1# & 2#)
+ * 
+ * 
+ * ATTENTION! Note that XXXX is your arduino device code on your PC
+ * ATTENTION! Don't forget to stop writing
+*/
