8896f42778fa31549a61825fe7893c0defd58de0
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2022-07-28 23:43:23 +0200

    Invio delle misurazioni verso il PC tramite interrupt USART

--- oscilloscope_avr.c
+++ oscilloscope_avr.c
@@ -13,6 +13,11 @@ char str_to_get[MAX_BUF];
 volatile uint8_t interrupt_flag = 0;
 volatile uint8_t measurement;
 
+char str_array_to_stream [256][5];
+uint8_t str_idx_usart = 0;
+uint8_t char_idx_usart = 0;
+uint8_t str_idx_adc = 0;
+
 void UART_init(void){
 	UBRR0H = (uint8_t)(MYUBRR>>8);
 	UBRR0L = (uint8_t)MYUBRR;
@@ -20,54 +25,59 @@ void UART_init(void){
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0);
 }
 
-void UART_putChar(uint8_t c){
-  while ( !(UCSR0A & (1<<UDRE0)) );
-  UDR0 = c;
+//Michele
+ISR(USART0_UDRE_vect){
+	if(str_array_to_stream[str_idx_usart][char_idx_usart]){
+		UDR0=str_array_to_stream[str_idx_usart][char_idx_usart];
+		char_idx_usart++;
+	}else{
+		if(str_idx_usart>255) {
+				str_idx_usart=0;
+				char_idx_usart=0;
+			}else {
+				str_idx_usart+=1;
+				char_idx_usart=0;
+				}
+		}
+	UCSR0B &= ~(1 << UDRIE0);
 }
 
-void UART_putString(uint8_t* buf){
-  while(*buf){
-    UART_putChar(*buf);
-    ++buf;
-  }
-}
-//Michele
 ISR(USART0_RX_vect){
 	uint8_t c=UDR0;
-	if(c == 'y'){	//y per far partire l'ADC
-			ADCSRA = 0xBF;
-			ADCSRA |= 0x40;
+	if(c == 'y'){	//y to start the ADC
+		ADCSRA = 0xBF;
+		ADCSRA |= 0x40;
 	}
-	if(c == 'n'){	//n per far fermare l'ADC
-			ADCSRA = 0x00;
-			ADCSRA = 0xBF;
+	if(c == 'n'){	//n to stop the ADC
+		ADCSRA = 0x00;
+		ADCSRA = 0xBF;
 	}
 }
 //Michele
 
-//Gabriele e Michele
+//Gabriele and Michele
 ISR(ADC_vect){
 	measurement=(uint8_t)ADC;
-	interrupt_flag=1;
+	if(str_idx_adc>255){
+		str_idx_adc=0;
+	}
+	sprintf(str_array_to_stream[str_idx_adc], "%d\n",(int)measurement);
+	str_idx_adc+=1;
+	UCSR0B |= (1 << UDRIE0);
 }
-//Gabriele e Michele
+//Gabriele and Michele
 
+//Gabriele and Michele
 int main(void){
-	//Gabriele e Michele
 	cli();
 	UART_init();
 	ADCSRB = 0x00;
 	ADMUX = 0x40;
 	sei();
-	while(1){
-		if(interrupt_flag){
-		sprintf(str_to_get, "%d\n",(int)measurement);
-		UART_putString((uint8_t*)str_to_get);
-		interrupt_flag=0;
-		}
-	}
-	//Gabriele e Michele
+	while(1){}
 }
+//Gabriele and Michele
+
 /* If you want to put measurements into a file
  * write this on the terminal.
  * 
@@ -84,8 +94,11 @@ int main(void){
  * 
  * 
  * 4.To see what are the measurements on the terminal
- * cat /dev/ttyXXXX (only after 1# & 2#)
+ * cat /dev/ttyXXXX (only after 1. & 2.)
  * 
+ * 5.Kill the process open once finished
+ * kill -9 $$$$$$
+ * (where $$$$$$ is the code of the process opened in 1.)
  * 
  * ATTENTION! Note that XXXX is your arduino device code on your PC
  * ATTENTION! Don't forget to stop writing
